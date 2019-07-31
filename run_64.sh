3a6ac257850670c8b8c45c014e603bc9b7342f98
ksteimel <ksteimel@iu.edu>
2019-07-31 15:18:49 +0000

    Added example running script

---
+++ run_64.sh
@@ -0,0 +1,1 @@
+python3 main.py --morph_dict ess_no_oov.64.vectors --data data/ess_coleman/ --lr 0.01 --cuda --optimizer adam --nhid 512 --epochs 1000 --when 200 --prediction_count_file determ_64_coleman_ess.preds
